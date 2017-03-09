a73421add91e0a4f4649a69a147bf2d38173b762
rpazda <richard.pazda@gmail.com>
2017-03-09 17:00:55 -0500

    Fixed minor JS Bugs, Added Search Form Mockup

--- DEV/mockups/main_mockup/index.js
+++ DEV/mockups/main_mockup/index.js
@@ -46,6 +46,7 @@ function initMap() {
     marker = new google.maps.Marker({ position: uniLocation, map: map }); //Create new marker on the map
 
     //creat dummy markers for map
+    /*
     m1 = new google.maps.Marker({ position: { lat: 28.602423, lng: -81.200343 }, map: map });
     m2 = new google.maps.Marker({ position: { lat: 28.602428, lng: -81.200347 }, map: map });
     m3 = new google.maps.Marker({ position: { lat: 28.602431, lng: -81.200339 }, map: map });
@@ -59,6 +60,28 @@ function initMap() {
     m11 = new google.maps.Marker({ position: { lat: 28.600001, lng: -81.201339 }, map: map });
     m12 = new google.maps.Marker({ position: { lat: 28.602396, lng: -81.195901 }, map: map });
     m13 = new google.maps.Marker({ position: { lat: 28.603027, lng: -81.204527 }, map: map });
+    */
+
+    STUN = new google.maps.Marker({ position: { lat: 28.601939931820066, lng: -81.20055198669434}, map: map });
+    LIB = new google.maps.Marker({ position: { lat: 28.60061176525329, lng: -81.20127028223578}, map: map });
+    HEC = new google.maps.Marker({ position: { lat: 28.60061176525329, lng: -81.19778341051642}, map: map });
+    VAB = new google.maps.Marker({ position: { lat:28.602787688936317, lng: -81.20321220156256}, map: map });
+    BHC = new google.maps.Marker({ position: { lat:28.602241357356093, lng: -81.20225733515326}, map: map });
+    CB1 = new google.maps.Marker({ position: { lat: 28.603691955654796, lng: -81.20052999254767}, map: map });
+    CB2 = new google.maps.Marker({ position: { lat: 28.60432305385778, lng: -81.20011156794135}, map: map });
+    NSC = new google.maps.Marker({ position: { lat: 28.603823827234365, lng: -81.20293325182502}, map: map });
+    BA1 = new google.maps.Marker({ position: { lat: 28.600734221029345, lng: -81.19929617640082}, map: map });
+    BA2 = new google.maps.Marker({ position: { lat: 28.600753060366827, lng: -81.19875973459784}, map: map });
+    ENG1 = new google.maps.Marker({ position: { lat: 28.600922614252315, lng: -81.1993396282196}, map: map });
+    ENG2 = new google.maps.Marker({ position: { lat: 28.60177979915234, lng: -81.19851297136847}, map: map });
+    HPA1 = new google.maps.Marker({ position: { lat: 28.603013756345504, lng: -81.19858807322089}, map: map });
+    HPA2 = new google.maps.Marker({ position: { lat: 28.60318330658323, lng: -81.19818037745063}, map: map });
+    MH = new google.maps.Marker({ position: { lat: 28.599142284810345, lng: -81.2023002504975}, map: map });
+    TA = new google.maps.Marker({ position: { lat: 28.599377779853693, lng: -81.2038779258728}, map: map });
+    TC1 = new google.maps.Marker({ position: { lat: 28.600291495625896, lng: -81.20040124651496}, map: map });
+    TC2 = new google.maps.Marker({ position: { lat:28.600578796366108, lng: -81.20051953194888}, map: map });
+
+    //var buildingCodes = [STUN, LIB, HEC, VAB, BHC, CB1, CB2, NSC, BA1,]
 
     //set details to display when marker clicked
     var spotDetails = "EXAMPLE TEXT"; //$('#info-window-content');
@@ -73,11 +96,11 @@ function initMap() {
     //not proud but it works
     //http://stackoverflow.com/questions/5613834/convert-string-to-variable-name-in-javascript
     for (i = 1; i < 14; i++) {
-        let mid = "m" + i;
+        mid = "m" + i;
         window[mid].addListener('click', function() {
             //$('#add-spot-button').addClass("disabled");
             infoWindow.open(map, window[mid]);
-        })
+        });
     }
 
     ///Creates a single marker at a time. New marker clears old marker
