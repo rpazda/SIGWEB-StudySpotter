8bb99414666369ff3be13b34329efa44717e671f
Sammy Israwi <sammy.israwi@gmail.com>
2017-03-11 20:32:26 -0500

    Added postedBy fields to Spots schema

--- models/spots.js
+++ models/spots.js
@@ -10,13 +10,16 @@ const spotsSchema = new mongoose.Schema({
         lat: {type: Number, required: true},
         lon: {type: Number, required: true}
     },
+    postedBy: {type: mongoose.Schema.Types.ObjectId }, //To get the actual user
+    postedByName: {type: String, required: true},   //If user is not defined
     isIndoor: {type: Boolean, required: true},
     soundLevel: {type: String, required: true, enum: ["Quiet", "Moderate", "Chatty", "Noisy"]},
     outletsCount: {type: String, required: true, enum: ["None", "1-3", "4-7", "8-10", ">10"]},
     seatAvailability: {type: String, required: true, enum: ["Sparce", "Sometimes", "Usually", "Always"]},
     seatCount: {type: String, required: true, enum: ["1-2", "3-4", "5-6", "7-8", ">8"]},
     coffeeNearby: {type: Boolean, required: true},
-    description: {type: String, required: true, trim: true}
+    description: {type: String, required: true, trim: true},
+
 });
 
 //Middleware error checking
